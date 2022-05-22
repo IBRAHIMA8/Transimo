b70de95a55395e0d814abb742894baffbebe6acd
IBRAHIMA <hadi.ibrahima8@gmail.com>
2022-05-22 22:30:49 +0100

    Placeholder

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_21_092533) do
+ActiveRecord::Schema.define(version: 2022_05_22_212123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2022_05_21_092533) do
     t.string "unconfirmed_email"
     t.boolean "admin", default: false
     t.string "name"
+    t.text "image"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true

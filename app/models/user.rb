e91c9b36fac9729f23b19b2d9df878a6ee68dd70
IBRAHIMA <hadi.ibrahima8@gmail.com>
2022-05-28 20:07:35 +0100

    init3

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,12 @@ class User < ApplicationRecord
   has_many :products
   has_many :messages, dependent: :destroy
   #has_many :conversations, dependent: :destroy
-  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :trackable, :validatable, :confirmable
+        :recoverable, :rememberable, :trackable, :validatable
+
+
+
 end
